6777e6ddc80949ae74c8228dcc9699feaf64a331
markbarm <markbarm@gmail.com>
2024-09-03 04:46:27 -0300

    construção do script

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const avanca = document.querySelectorAll('.btn-proximo');
  avanca.forEach(button => {
     button.addEventListener('click',function(){
         const atual = document.querySelectorAll('.ativo');
-        const proximopasso = 'passo-' + this.getAttribute('data-proximo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
